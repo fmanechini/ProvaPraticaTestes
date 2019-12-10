559c978a8986cc3d82be974e433198618303989e
brenopestanac <brenopestana28@gmail.com>
2019-12-10 14:19:55 -0300

    Form atualizar e table lista
    Foi feito a estilização do formulario para atualizar e da listagem de cliente

--- templates/atualizar.php
+++ templates/atualizar.php
@@ -1,66 +1,75 @@
 <?php
-	include 'db.php';
-	
-	$sql = "SELECT * FROM colecionavel WHERE id=$_GET[id]";
-	
-  	$con = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+include 'db.php';
+
+$sql = "SELECT * FROM colecionavel WHERE id=$_GET[id]";
+
+$con = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Atualizar Cliente</title>
-	</head>
-	<body>
-		<h1>Atualizar Cliente</h1>
-		<hr>
-			<form method="post" action="">
-				<?php if($dados = mysqli_fetch_array($con)) { ?>
-				<table>
-					<tr>
-						<td>Nome:</td>
-						<td><input type="text" name="nome" value=<?php echo $dados['nome']; ?>></td>
-					</tr>
-					<tr>
-						<td>Tipo:</td>
-						<td><input type="text" name="tipo" value=<?php echo $dados['tipo']; ?>></td>
-					</tr>
-					<tr>
-						<td>Tempo:</td>
-						<td><input type="text" name="tempo" value=<?php echo $dados['tempo']; ?>></td>
-					</tr>
-					<tr>
-						<td>Proprietário:</td>
-						<td><input type="text" name="id_colecionador" value=<?php echo $dados['id_colecionador']; ?>></td>
-					</tr>
-					<tr>
-						<td>Detalhes:</td>
-						<td><input type="text" name="detalhes" value=<?php echo $dados['detalhes']; ?>></td>
-					</tr>
-					<tr>
-						<td>Quantidade:</td>
-						<td><input type="text" name="quantidade" value=<?php echo $dados['quantidade']; ?>></td>
-					</tr>
-				</table>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-				<?php
-					if (!empty($_POST['nome'])and($_POST['tipo'])and($_POST['tempo'])and($_POST['id_colecionador'])and($_POST['detalhes'])and($_POST['quantidade'])){
-                        $nome 	= $_POST['nome'];
-                        $tipo 	= $_POST['tipo'];
-                        $tempo = $_POST['tempo'];
-                        $id_colecionador = $_POST['id_colecionador'];
-                        $detalhes = $_POST['detalhes'];
-                        $quantidade 	= $_POST['quantidade'];
-						$id = $_GET['id'];
-						$sql = mysqli_query($conexao,"UPDATE colecionador SET nome='$nome', tipo='$tipo', tempo='$tempo', id_colecionador='$id_colecionador', detalhes='$detalhes', quantidade='$quantidade'WHERE id=".$id);
-						header("Location: http://localhost/crud/templates/lista.php");
-				}
-				?>
-				
-				
-				<input type="submit" value="Atualizar">
-				<?php } ?>
-			</form>
-		<a href="lista.php"><input type="submit" value="voltar"></a>
-	</body>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Atualizar Cadastro</title>
+</head>
+
+<div class="container" style="margin-top:35px;">
+    <h1>Atualizar Cadastros</h1>
+
+    <hr>
+    <div class="form-group">
+        <label>Nome:</label>
+        <input type="text" class="form-control" placeholder="" name="nome">
+    </div>
+    <div class="form-group">
+        <label>Tipo:</label>
+        <input type="select" class="form-control" placeholder="" name="tipo">
+    </div>
+    <div class="form-group">
+        <label>Tempo (em meses):</label>
+        <input type="int" class="form-control" placeholder="">
+    </div>
+
+    <div class="form-group">
+        <label>Quantidade:</label>
+        <input type="int" name="quantidade" class="form-control" placeholder="">
+    </div>
+    <div class="form-group">
+        <label>Detalhes:</label>
+        <textarea class="form-control" name="detalhes" id="exampleFormControlTextarea1"
+                  placeholder="Insira os detalhes do seu colecionavel" rows="3"></textarea>
+    </div>
+
+
+    <input type="submit" class="btn btn-primary btn-lg" value="Salvar">
+    </form>
+    <a href="../index.php"><input type="submit" class="btn btn-secondary btn-lg" value="voltar"></a>
+
+</div>
+
+<?php
+if (!empty($_POST['nome']) and ($_POST['tipo']) and ($_POST['tempo']) and ($_POST['id_colecionador']) and ($_POST['detalhes']) and ($_POST['quantidade'])) {
+    $nome = $_POST['nome'];
+    $tipo = $_POST['tipo'];
+    $tempo = $_POST['tempo'];
+    $id_colecionador = $_POST['id_colecionador'];
+    $detalhes = $_POST['detalhes'];
+    $quantidade = $_POST['quantidade'];
+    $id = $_GET['id'];
+    $sql = mysqli_query($conexao, "UPDATE colecionador SET nome='$nome', tipo='$tipo', tempo='$tempo', id_colecionador='$id_colecionador', detalhes='$detalhes', quantidade='$quantidade'WHERE id=" . $id);
+    header("Location: http://localhost/crud/templates/lista.php");
+}
+?>
+
+
+
+<?php ?>
+</form>
+</body>
 </html>
--- templates/cadastro.php
+++ templates/cadastro.php
@@ -5,74 +5,80 @@ include 'db.php';
 <html lang="en">
 
 <head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-	<title>CADASTRO</title>
+    <title>CADASTRO</title>
 </head>
 
 <body>
-	<div class="container" style="margin-top:35px;">
-		<h1>Cadastro de Colecionáveis</h1>
+<div class="container" style="margin-top:35px;">
+    <h1>Cadastro de Colecionáveis</h1>
 
-		<hr>
-		<div class="form-group">
-			<label>Nome:</label>
-			<input type="text" class="form-control" placeholder="Insira seu nome" name="nome">
-		</div>
-		<div class="form-group">
-			<label>Tipo:</label>
-			<input type="select" class="form-control" placeholder="Insira o tipo do seu colecionavel" name="tipo">
-		</div>
-		<div class="form-group">
-			<label>Tempo (em meses):</label>
-			<input type="int" class="form-control" placeholder="Insira a idade do seu item">
-		</div>
+    <hr>
+    <div class="form-group">
+        <label>Nome:</label>
+        <input type="text" class="form-control" placeholder="Insira seu nome" name="nome">
+    </div>
+    <div class="form-group">
+        <label>Tipo:</label>
+        <input type="select" class="form-control" placeholder="Insira o tipo do seu colecionavel" name="tipo">
+    </div>
+    <div class="form-group">
+        <label>Tempo (em meses):</label>
+        <input type="int" class="form-control" placeholder="Insira a idade do seu item">
+    </div>
 
-		<div class="form-group">
-			<label>Quantidade:</label>
-			<input type="int" name="quantidade" class="form-control" placeholder="Insira a quantidade do seu item">
-		</div>
-		<div class="form-group">
-			<label>Detalhes:</label>
-			<textarea class="form-control" name="detalhes" id="exampleFormControlTextarea1" placeholder="Insira os detalhes do seu colecionavel" rows="3"></textarea>
-		</div>
+    <div class="form-group">
+        <label>Quantidade:</label>
+        <input type="int" name="quantidade" class="form-control" placeholder="Insira a quantidade do seu item">
+    </div>
+    <div class="form-group">
+        <label>Detalhes:</label>
+        <textarea class="form-control" name="detalhes" id="exampleFormControlTextarea1"
+                  placeholder="Insira os detalhes do seu colecionavel" rows="3"></textarea>
+    </div>
 
 
-		<input type="submit" class="btn btn-primary btn-lg" value="Salvar">
-		</form>
-		<a href="../index.php"><input type="submit" class="btn btn-secondary btn-lg" value="voltar"></a>
+    <input type="submit" class="btn btn-primary btn-lg" value="Salvar">
+    </form>
+    <a href="../index.php"><input type="submit" class="btn btn-secondary btn-lg" value="voltar"></a>
 
-	</div>
-	<?php //inserindo as informações do cliente no banco de dados.
-	if (!empty($_POST['nome']) and ($_POST['tipo']) and ($_POST['tempo']) and ($_POST['id_colecionador']) and ($_POST['detalhes']) and ($_POST['quantidade'])) {
-		$nome 	= $_POST['nome'];
-		$tipo 	= $_POST['tipo'];
-		$tempo = $_POST['tempo'];
-		$id_colecionador = $_POST['id_colecionador'];
-		$detalhes = $_POST['detalhes'];
-		$quantidade 	= $_POST['quantidade'];
-		$erro = 0;
-		$sql = mysqli_query($conexao, "INSERT INTO colecionavel(nome, tipo, tempo, id_colecionador, detalhes, quantidade)
+</div>
+<?php //inserindo as informações do cliente no banco de dados.
+if (!empty($_POST['nome']) and ($_POST['tipo']) and ($_POST['tempo']) and ($_POST['id_colecionador']) and ($_POST['detalhes']) and ($_POST['quantidade'])) {
+    $nome = $_POST['nome'];
+    $tipo = $_POST['tipo'];
+    $tempo = $_POST['tempo'];
+    $id_colecionador = $_POST['id_colecionador'];
+    $detalhes = $_POST['detalhes'];
+    $quantidade = $_POST['quantidade'];
+    $erro = 0;
+    $sql = mysqli_query($conexao, "INSERT INTO colecionavel(nome, tipo, tempo, id_colecionador, detalhes, quantidade)
 					VALUES('$nome','$tipo', '$tempo', '$id_colecionador', '$detalhes', '$quantidade')");
-		//header("Location: http://localhost/crud/templates/lista.php");
-		echo '<h1>CADASTRADO COM SUCESSO!</h1>';
-	}
-	// $sql = mysql_query("DELETE FROM colecionavel WHERE id > 2");
-	?>
+    //header("Location: http://localhost/crud/templates/lista.php");
+    echo '<h1>CADASTRADO COM SUCESSO!</h1>';
+}
+// $sql = mysql_query("DELETE FROM colecionavel WHERE id > 2");
+?>
 
 
-
-
-	<!-- Optional JavaScript -->
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
